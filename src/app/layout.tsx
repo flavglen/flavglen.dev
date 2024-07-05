75b1d46939e6b8b2502f96ca2e3aafd9ab6b7c4b
glen pais <glen.pais@loblaw.ca>
2024-07-05 16:13:51 -0400

    track subdomain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} flex flex-col h-screen`}>
           <MainHeader />
-          {children}
+          <>{children}</>
         </body>
     </html>
   );
