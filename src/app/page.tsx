7779e03705bfd7c974130bbd8d6b458d45221fb5
glen pais <glen.pais@loblaw.ca>
2024-07-05 16:32:04 -0400

    add controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,61 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar"
+import { useState } from "react";
 
 export default function Home() {
+  const [date, setDate] = useState<Date | undefined>(new Date())
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Card className="w-full">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle>Add Expense</CardTitle>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <ToggleGroup type="single" className="flex flex-wrap">
+          <ToggleGroupItem value="nofrills" aria-label="Toggle bold">
+            No Frills
+          </ToggleGroupItem>
+          <ToggleGroupItem value="Freshco" aria-label="Toggle italic">
+            Freshco
+          </ToggleGroupItem>
+          <ToggleGroupItem value="batala" aria-label="Toggle underline">
+            batala
+          </ToggleGroupItem>
+          <ToggleGroupItem value="walmart" aria-label="Toggle underline">
+            walmart
+          </ToggleGroupItem>
+          <ToggleGroupItem value="tim" aria-label="Toggle underline">
+            tim
+          </ToggleGroupItem>
+          <ToggleGroupItem value="yal" aria-label="Toggle underline">
+            yal
+          </ToggleGroupItem>
+          <ToggleGroupItem value="fusion" aria-label="Toggle underline">
+            fusion
+          </ToggleGroupItem>
+          <ToggleGroupItem value="jianhing" aria-label="Toggle underline">
+            jian Hing
+          </ToggleGroupItem>
+        </ToggleGroup>
+
+        <Input  type="number"/>
+
+        <Calendar
+            mode="single"
+            disabled={(date) =>
+              date > new Date() || date < new Date("1900-01-01")
+            }
+            initialFocus
+          />
         </CardContent>
         <CardFooter>
-          <p>Card Footer</p>
+          <Button>Save</Button>
         </CardFooter>
       </Card>
     </main>
