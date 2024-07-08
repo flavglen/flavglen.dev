49d011d1934264e2b5aa313507c004a3358867c0
glen pais <glen.pais@loblaw.ca>
2024-07-07 22:09:54 -0400

    fix config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 "use client"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore, doc } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'; // Import getAuth from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
